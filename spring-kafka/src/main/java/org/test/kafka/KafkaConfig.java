655a4dcd88b960283b0e99724d66bab1164ea678
thbspan <thbspan@163.com>
2020-05-28 19:01:46 +0800

    add spring-kafka demo

---
+++ spring-kafka/src/main/java/org/test/kafka/KafkaConfig.java
@@ -0,0 +1,39 @@
+package org.test.kafka;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ApplicationRunner applicationRunner(KafkaTemplate<String, String> kafkaTemplate) {
+        return args -> {
+            new Thread(() -> {
+                // 发送消息
+                for (int i = 0; i < 10; i++) {
+                    ListenableFuture<SendResult<String, String>> send = kafkaTemplate.send("topic-test", Integer.toString(i), "test data " + i);
+                    send.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+                        @Override
+                        public void onSuccess(SendResult<String, String> result) {
+                            System.out.println("send meg=" + result.getProducerRecord() + " success");
+                        }
+
+                        @Override
+                        public void onFailure(Throwable throwable) {
+                            throwable.printStackTrace();
+                        }
+                    });
+                }
+            }).start();
+
+
+        };
+    }
+}
---
+++ spring-kafka/src/main/java/org/test/kafka/SimpleConsumerListener.java
@@ -0,0 +1,33 @@
+package org.test.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleConsumerListener {
+
+    /**
+     * https://www.jianshu.com/p/a64defb44a23
+     * <br/>
+     * listen1(String data)
+     *
+     * listen2(ConsumerRecord<K,V> data)
+     *
+     * listen3(ConsumerRecord<K,V> data, Acknowledgment acknowledgment)
+     *
+     * listen4(ConsumerRecord<K,V> data, Acknowledgment acknowledgment, Consumer<K,V> consumer)
+     *
+     * listen5(List<String> data)
+     *
+     * listen6(List<ConsumerRecord<K,V>> data)
+     *
+     * listen7(List<ConsumerRecord<K,V>> data, Acknowledgment acknowledgment)
+     *
+     * listen8(List<ConsumerRecord<K,V>> data, Acknowledgment acknowledgment, Consumer<K,V> consumer)
+     */
+    @KafkaListener(topics = {"topic-test"})
+    public void listen(ConsumerRecord<String, String> record) {
+        System.out.println("rec message: " + record);
+    }
+}
